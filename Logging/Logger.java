76dd4801c809e122ec619dbafcec2b57db13458f
Shaen Deimling <ShaenDeimling@users.noreply.github.com>
2017-06-08 22:49:28 -0400

    Comments

--- Logging/Logger.java
+++ Logging/Logger.java
@@ -1,18 +1,49 @@
 package Logging;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * Used to handle logging errors.
+ */
 public final class Logger {
+	/**
+	 * The user's home directory.
+	 */
 	private static File homeDirectory = null;
+	
+	/**
+	 * The file to log to.
+	 */
 	private static File logFile = null;
+	
+	/**
+	 * The log file's file writer.
+	 */
 	private static FileWriter fw = null;
+	
+	/**
+	 * Buffered wrapped for the file writer.
+	 */
 	private static BufferedWriter bw = null;
+	
+	/**
+	 * Print wrapper for the buffered writer.
+	 */
 	private static PrintWriter pw = null;
+	
+	/**
+	 * True iff the file has been initialized for writing.
+	 */
 	private static boolean initialized = false;
 	
+	/**
+	 * Logs a message to the log file. If this fails, prints to standard out.
+	 * @param message The message to be logged.
+	 */
 	public static void log(String message) {
 		if (!initialized) {
 			try {
@@ -36,6 +67,9 @@ public final class Logger {
 		}
 	}
 
+	/**
+	 * Tries to obtain the file for writing.
+	 */
 	private static void initialize() throws IOException {
 		homeDirectory = new File(System.getProperty("user.home")).getAbsoluteFile();
 		logFile = homeDirectory.toPath().resolve("awed.log").toFile();
