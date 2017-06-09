eae47d7c2cad15b79b3c879b5df701e3be2e9e37
Shaen Deimling <ShaenDeimling@users.noreply.github.com>
2017-06-08 23:40:27 -0400

    Comments

--- MusicPlayer/BassBall.java
+++ MusicPlayer/BassBall.java
@@ -1,8 +1,8 @@
 package MusicPlayer;
 
-//loudness = brightness?
-//base = shake+speed?
-
+/**
+ * Encapsulates the data needed for a bass-ball.
+ */
 final class BassBall {
 	protected static double radius = 8.0;
 	protected static double diameter = radius * 2.0;
@@ -11,6 +11,12 @@ final class BassBall {
 	protected double x = 0.5;
 	protected double y = 0.5;
 
+	/**
+	 * Create a new bass-ball with the specified initial direction.
+	 * 
+	 * @param direction
+	 *            The initial direction in radians.
+	 */
 	protected BassBall(double direction) {
 		this.direction = direction;
 	}
--- MusicPlayer/FadeButton.java
+++ MusicPlayer/FadeButton.java
@@ -1,16 +1,38 @@
 package MusicPlayer;
+
 import javafx.event.EventHandler;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Represents a simple button that glows when clicked.
+ */
 final class FadeButton extends Rectangle {
+	/**
+	 * The opacity immediately after clicked.
+	 */
 	static protected double maxOpacity = 0.1;
+	
+	/**
+	 * The current opacity.
+	 */
 	protected double opacity = 0.0;
+	
+	/**
+	 * The button's tooltip.
+	 */
 	protected Tooltip tt = null;
+	
+	/**
+	 * The button's color.
+	 */
 	protected Color color = Color.color(0.3, 0.3, 0.3);
 	
+	/**
+	 * Create a button with a tooltip.
+	 */
 	protected FadeButton(String tooltip) {
 		setFill(Color.TRANSPARENT);
 		tt = new Tooltip(tooltip);
@@ -23,11 +45,20 @@ final class FadeButton extends Rectangle {
 		});
 	}
 	
+	/**
+	 * Create a colored button with a tooltip.
+	 * @param tooltip
+	 * @param c
+	 */
 	protected FadeButton(String tooltip, Color c) {
 		this(tooltip);
 		color = c;
 	}
 	
+	/**
+	 * Set this button's tooltip.
+	 * @param tooltip The new tooltip.
+	 */
 	protected void setTooltip(String tooltip) {
 		Tooltip.uninstall(this, tt);
 		tt = new Tooltip(tooltip);
